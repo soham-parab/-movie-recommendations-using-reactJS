0adaa302f717aea52b3f0f248b4ae5e7272ac28b
soham-parab <57428835+soham-parab@users.noreply.github.com>
2020-12-20 18:04:02 +0530

    listed all the movies

--- src/App.js
+++ src/App.js
@@ -5,42 +5,52 @@ import "./styles.css";
 export default function App() {
   const movies = {
     ScienceFiction: [
-      { name: "Movie1", Imdb: "9/10" },
-      { name: "Movie2", Imdb: "6/10" },
-      { name: "Movie3", Imdb: "5/10" },
-      { name: "Movie4", Imdb: "8/10" },
-      { name: "Movie5", Imdb: "7/10" }
+      { name: "Arrival", Imdb: "7.9/10" },
+      { name: "2001: A Space Odyssey", Imdb: "8.3/10" },
+      { name: "Source Code", Imdb: "7.5/10" },
+      { name: "Interstellar", Imdb: "8.6/10" },
+      { name: "Her", Imdb: "8/10" }
     ],
     Drama: [
-      { name: "Movie6", Imdb: "6/10" },
-      { name: "Movie7", Imdb: "7/10" },
-      { name: "Movie8", Imdb: "9/10" },
-      { name: "Movie9", Imdb: "8/10" },
-      { name: "Movie10", Imdb: "9/10" }
+      { name: "Pulp Fiction", Imdb: "8.9/10" },
+      { name: "The Big Short", Imdb: "7.8/10" },
+      { name: "The Wolf of Wall Street", Imdb: "8.2/10" },
+      { name: "The Shawshank Redemption", Imdb: "9.3/10" },
+      { name: "12 Angry Men", Imdb: "9/10" }
     ],
+
     Action: [
-      { name: "Movie11", Imdb: "7/10" },
-      { name: "Movie12", Imdb: "6/10" },
-      { name: "Movie13", Imdb: "7/10" },
-      { name: "Movie14", Imdb: "8/10" },
-      { name: "Movie15", Imdb: "9/10" }
+      { name: "The Departed", Imdb: "8.5/10" },
+      { name: "Scarface", Imdb: "8.3/10" },
+      { name: "The Revenant", Imdb: "8/10" },
+      { name: "Drive", Imdb: "7.8/10" },
+      { name: "Mad Max Fury Road", Imdb: "8.1/10" }
     ],
     Comedy: [
-      { name: "Movie16", Imdb: "10/10" },
-      { name: "Movie17", Imdb: "9/10" },
-      { name: "Movie18", Imdb: "9/0" }
+      { name: "Borat", Imdb: "7.3/10" },
+      { name: "The Scary Movie Saga", Imdb: "7.2/10" },
+      { name: "21/22 Jump Street", Imdb: "7.2/0" }
     ],
 
     Romance: [
-      { name: "Movie19", Imdb: "10/10" },
-      { name: "Movie20", Imdb: "9/10" },
-      { name: "Movie21", Imdb: "9/10" },
-      { name: "Movie22", Imdb: "8/10" }
+      { name: "500 Days of Summer", Imdb: "7.7/10" },
+      { name: "The Before Series", Imdb: "8.1/10" },
+      { name: "Manchester By the Sea", Imdb: "7.8/10" },
+      { name: "La La Land", Imdb: "8/10" },
+      { name: "Eternal Sunshine of the Spotless Mind", Imdb: "8.3/10" }
+    ],
+    Crime: [
+      { name: "Seven", Imdb: "8.6/10" },
+      { name: "Prisoners", Imdb: "8.1/10" },
+      { name: "Godfather", Imdb: "9.2/10" },
+      { name: "The Usual Suspects", Imdb: "8.5/10" },
+      { name: "The Place Beyond the Pines", Imdb: "7.3/10" },
+      { name: "Zodiac", Imdb: "7.7/10" }
     ],
     Thriller: [
-      { name: "Movie23", Imdb: "10/10" },
-      { name: "Movie24", Imdb: "9/10" },
-      { name: "Movie25", Imdb: "9/10" }
+      { name: "Parasite", Imdb: "8.6/10" },
+      { name: "The Platform", Imdb: "7/10" },
+      { name: "A Quiet Place", Imdb: "7.5/10" }
     ]
   };
 
